5b27203e2c0e96739ba2275313dcfa682918a97e
PAULACASTILLEJOBRAVO <96077624+PAULACASTILLEJOBRAVO@users.noreply.github.com>
2022-10-12 09:50:40 +0200

    Clases originales

---
+++ TitulosPropiosUCLM/src/negocio/controllers/GestorConsultas.java
@@ -0,0 +1,43 @@
+package negocio.controllers;
+
+import java.util.*;
+
+import negocio.entities.*;
+
+public class GestorConsultas {
+
+	//asdfasfasfasff
+	
+	/**
+	 * 
+	 * @param tipo
+	 * @param fechaInicio
+	 * @param fechaFin
+	 */
+	public List<CursoPropio> consultarIngresos(TipoCurso tipo, Date fechaInicio, Date fechaFin) {
+		// TODO - implement GestorConsultas.consultarIngresos
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param estadoCurso
+	 * @param fechaInicio
+	 * @param fechaFin
+	 */
+	public List<CursoPropio> consultarEstadoCursos(EstadoCurso estadoCurso, Date fechaInicio, Date fechaFin) {
+		// TODO - implement GestorConsultas.consultarEstadoCursos
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param fechaInicio
+	 * @param fechaFin
+	 */
+	public List<CursoPropio> listarEdicionesCursos(Date fechaInicio, Date fechaFin) {
+		// TODO - implement GestorConsultas.listarEdicionesCursos
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ TitulosPropiosUCLM/src/negocio/entities/Materia.java
@@ -0,0 +1,13 @@
+package negocio.entities;
+
+import java.util.*;
+
+public class Materia {
+
+	Profesor responsable;
+	private String nombre;
+	private double horas;
+	private Date fechaInicio;
+	private Date fechaFin;
+
+}
