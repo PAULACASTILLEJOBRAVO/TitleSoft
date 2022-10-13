e30c909c5fe2e0c376dd0cfc53d0e2d1ec511f5f
Virginis <virginia.lopez9@alu.uclm.es>
2022-10-13 11:13:36 +0200

    get, set y constructores

--- TitulosPropiosUCLM/src/negocio/entities/ProfesorUCLM.java
+++ TitulosPropiosUCLM/src/negocio/entities/ProfesorUCLM.java
@@ -7,5 +7,39 @@ public class ProfesorUCLM extends Profesor {
 	private String nombre;
 	private String apellidos;
 	private String dni;
+	public Centro getCentroAdscripcion() {
+		return centroAdscripcion;
+	}
+	public void setCentroAdscripcion(Centro centroAdscripcion) {
+		this.centroAdscripcion = centroAdscripcion;
+	}
+	public CategoriaProfesor getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(CategoriaProfesor categoria) {
+		this.categoria = categoria;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public ProfesorUCLM(String dni, String nombre, String apellidos, boolean doctor) {
+		super(dni, nombre, apellidos, doctor);
+		// TODO Auto-generated constructor stub
+	}
 
 }
