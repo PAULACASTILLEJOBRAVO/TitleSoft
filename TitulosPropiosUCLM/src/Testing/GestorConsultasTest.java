8149ff08c8c68b46fd0e226b09a701cc4ffafc3d
Virginis <virginia.lopez9@alu.uclm.es>
2022-12-18 20:24:20 +0100

    Error 3

--- TitulosPropiosUCLM/src/Testing/GestorConsultasTest.java
+++ TitulosPropiosUCLM/src/Testing/GestorConsultasTest.java
@@ -2,20 +2,14 @@ package Testing;
 
 import static org.junit.Assert.*;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.Collection;
+import java.text.SimpleDateFormat;
 import java.util.Date;
-
 import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import negocio.entities.CursoPropio;
-import persistencia.CursoPropioDAO;
 
 public class GestorConsultasTest {
 
@@ -51,8 +45,101 @@ public class GestorConsultasTest {
 	@Test
 	public void consultarIngresos() {
 		
-		Date d = new Date(2022-12-22);
+		Date dInicio = new Date(2023-12-03);
+		Date dFin = new Date(2024-05-07);
+		 SimpleDateFormat getYearFormat = new SimpleDateFormat("yyyy");
+		 SimpleDateFormat getMonthFormat = new SimpleDateFormat("mm"); 
+	     String anioInicio = getYearFormat.format(dInicio);
+	     String mesInicio = getMonthFormat.format(dInicio);
+
+	     String anioFin = getYearFormat.format(dFin);
+	     String mesFin = getMonthFormat.format(dInicio);
+	     Date fechaActual = new Date();
+	     String anioActual = getYearFormat.format(fechaActual);
+		assertTrue((Integer.parseInt(anioInicio)< Integer.parseInt(anioActual) && Integer.parseInt(anioFin)< Integer.parseInt(anioActual)
+	    		 && Integer.parseInt(mesInicio) != 6 && Integer.parseInt(mesInicio) != 7 && Integer.parseInt(mesFin) != 6 && Integer.parseInt(mesFin) != 7));
 		
 	}
+	@Test
+	public void consultarIngresos1() {
+		
+		Date dInicio = new Date(2023-06-14);
+		Date dFin = new Date(2039-06-04);
+		 SimpleDateFormat getYearFormat = new SimpleDateFormat("yyyy");
+		 SimpleDateFormat getMonthFormat = new SimpleDateFormat("mm"); 
+	     String anioInicio = getYearFormat.format(dInicio);
+	     String mesInicio = getMonthFormat.format(dInicio);
 
+	     String anioFin = getYearFormat.format(dFin);
+	     String mesFin = getMonthFormat.format(dInicio);
+	     Date fechaActual = new Date();
+	     String anioActual = getYearFormat.format(fechaActual);
+		assertTrue((Integer.parseInt(anioInicio)< Integer.parseInt(anioActual) && Integer.parseInt(anioFin)< Integer.parseInt(anioActual)
+	    		 && Integer.parseInt(mesInicio) != 6 && Integer.parseInt(mesInicio) != 7 && Integer.parseInt(mesFin) != 6 && Integer.parseInt(mesFin) != 7));
+		
+	}
+	@Test
+	public void consultarIngresos2() {
+	
+	Date dInicio = new Date(2023-07-28);
+	Date dFin = new Date(1999-10-16);
+	 SimpleDateFormat getYearFormat = new SimpleDateFormat("yyyy");
+	 SimpleDateFormat getMonthFormat = new SimpleDateFormat("mm"); 
+     String anioInicio = getYearFormat.format(dInicio);
+     String mesInicio = getMonthFormat.format(dInicio);
+
+     String anioFin = getYearFormat.format(dFin);
+     String mesFin = getMonthFormat.format(dInicio);
+     Date fechaActual = new Date();
+     String anioActual = getYearFormat.format(fechaActual);
+	assertTrue((Integer.parseInt(anioInicio)< Integer.parseInt(anioActual) && Integer.parseInt(anioFin)< Integer.parseInt(anioActual)
+    		 && Integer.parseInt(mesInicio) != 6 && Integer.parseInt(mesInicio) != 7 && Integer.parseInt(mesFin) != 6 && Integer.parseInt(mesFin) != 7));
+	
+}
+	@Test
+	public void consultarIngresos3() {
+	
+	Date dInicio = new Date(1987-04-07);
+	Date dFin = new Date(2024-07-27);
+	 SimpleDateFormat getYearFormat = new SimpleDateFormat("yyyy");
+	 SimpleDateFormat getMonthFormat = new SimpleDateFormat("mm"); 
+     String anioInicio = getYearFormat.format(dInicio);
+     String mesInicio = getMonthFormat.format(dInicio);
+
+     String anioFin = getYearFormat.format(dFin);
+     String mesFin = getMonthFormat.format(dInicio);
+     Date fechaActual = new Date();
+     String anioActual = getYearFormat.format(fechaActual);
+     System.out.println("El año actual es: " + anioActual);
+     System.out.println("El año de inicio es: " + anioInicio);
+     System.out.println("El año fin es: " + anioFin);
+	assertTrue((Integer.parseInt(anioInicio)< Integer.parseInt(anioActual) && Integer.parseInt(anioFin)< Integer.parseInt(anioActual)
+    		 && Integer.parseInt(mesInicio) != 6 && Integer.parseInt(mesInicio) != 7 && Integer.parseInt(mesFin) != 6 && Integer.parseInt(mesFin) != 7));
+	
+}
+	@Test
+	public void consultarIngresos4() {
+	
+	Date dInicio1 = new Date(2067-03-14);
+	Date dFin = new Date(2024-11-05);
+	 SimpleDateFormat getYearFormat = new SimpleDateFormat("yyyy");
+	 SimpleDateFormat getMonthFormat = new SimpleDateFormat("mm"); 
+     String anioInicio = getYearFormat.format(dInicio1);
+     String mesInicio = getMonthFormat.format(dInicio1);
+
+     String anioFin = getYearFormat.format(dFin);
+     String mesFin = getMonthFormat.format(dInicio1);
+     Date fechaActual = new Date();
+     String anioActual = getYearFormat.format(fechaActual);
+    
+     
+
+	assertTrue((Integer.parseInt(anioInicio)< Integer.parseInt(anioActual) && Integer.parseInt(anioFin)< Integer.parseInt(anioActual)
+    		 && Integer.parseInt(mesInicio) != 6 && Integer.parseInt(mesInicio) != 7 && Integer.parseInt(mesFin) != 6 && Integer.parseInt(mesFin) != 7));
+	
+}
+	@Test
+	public void listarCursosEstados() {
+		
+	}
 }
