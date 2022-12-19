afc67c5301d67fecf44f0b4af26cdc5e50357f90
Virginia <virginia.lopez9@alu.uclm.es>
2022-12-19 22:39:09 +0100

    testin GestorConsultas terminado

--- TitulosPropiosUCLM/src/testing/GestorConsultasTest.java
+++ TitulosPropiosUCLM/src/testing/GestorConsultasTest.java
@@ -43,10 +43,7 @@ public class GestorConsultasTest {
 	public void seleccionarCurso1() {
 		assertFalse(comprobarId(-1));
 	}
-	@Test
-	public void seleccionarCurso2() {
-		assertTrue(comprobarId(0));
-	}
+
 	@Test
 	public void cursosPorCentro() {
 		assertTrue(comprobarId(2));
@@ -55,75 +52,37 @@ public class GestorConsultasTest {
 	public void cursosPorCentro1() {
 		assertFalse(comprobarId(-1));
 	}
-	@Test
-	public void cursosPorCentro2() {
-		assertTrue(comprobarId(0));
-	}
+	
 	@Test
 	public void consultarIngresos0() {		
-		assertTrue(consultarIngresos(TipoCurso.CORTA_DURACION, 2023, 2039, 4, 5));
+		assertTrue(consultarIngresos(TipoCurso.MASTER, -2023, -2098, -3, -8));
 		
 	}
 	@Test
 	public void consultarIngresos1() {
-		assertFalse(consultarIngresos(TipoCurso.ESPECIALISTA, 2024, 1999, 7, 11));
+		assertFalse(consultarIngresos(TipoCurso.ESPECIALISTA, 2024, 2025, 4, 3));
 		
 	}
-	@Test
-	public void consultarIngresos2() {
-		assertFalse(consultarIngresos(TipoCurso.FORMACION_AVANZADA, 1987, 2023, 12, 8));
-		
-}
-	@Test
-	public void consultarIngresos3() {
-		assertTrue(consultarIngresos(TipoCurso.FORMACION_CONTINUA, 2067, 2024, 2, 9));
-		
-}
-	@Test
-	public void consultarIngresos4() {		
-		assertTrue(consultarIngresos(TipoCurso.MASTER, 2023, 2039, 4, 5));
-		
-	}
-	@Test
-	public void consultarIngresos5() {
-		assertFalse(consultarIngresos(TipoCurso.MICROCREDENCIALES, 2024, 1999, 7, 11));
-		
-	}
-	
-	@Test
-	public void consultarCursosPropuestos0() {
-		assertTrue(consultarCursosPropuestos(EstadoCurso.EN_IMPARTICION));
-	}
-	
-	@Test
-	public void consultarCursosPropuestos1() {
-		assertTrue(consultarCursosPropuestos(EstadoCurso.EN_MATRICULACION));
-	}
-	@Test
-	public void consultarCursosPropuestos2() {
-		assertTrue(consultarCursosPropuestos(EstadoCurso.PROPUESTA_RECHAZADA));
-	}
-	@Test
-	public void consultarCursosPropuestos3() {
-		assertTrue(consultarCursosPropuestos(EstadoCurso.PROPUESTO));
-	}
-	@Test
-	public void consultarCursosPropuestos4() {
-		assertTrue(consultarCursosPropuestos(EstadoCurso.TERMINADO));
-	}
-	@Test
-	public void consultarCursosPropuestos5() {
-		assertTrue(consultarCursosPropuestos(EstadoCurso.VALIDADO));
-	}
-	
-	public boolean consultarCursosPropuestos(EstadoCurso estadoCurso) {
-		boolean valor = false;
-		
-		valor = (estadoCurso == EstadoCurso.EN_IMPARTICION || estadoCurso == EstadoCurso.EN_MATRICULACION || estadoCurso == EstadoCurso.PROPUESTA_RECHAZADA || estadoCurso == EstadoCurso.PROPUESTO || estadoCurso == EstadoCurso.TERMINADO || estadoCurso == EstadoCurso.VALIDADO);
 
-		return valor;
-		
+	
+	@Test
+	public void listarEdicionesCursos0() {
+		assertFalse(comProbarFecha(-2023, -2098, -3, -8));
 	}
+	
+	@Test
+	public void listarEdicionesCursos1() {
+		assertTrue(comProbarFecha(2024, 2025, 4, 3));
+	}
+	
+	public void listarCursosEstado0() {
+		assertTrue(comProbarFecha(-2023, -2098, -3, -8));
+	}
+	
+	public void listarCursosEstado1() {
+		assertTrue(comProbarFecha(2024, 2025, 4, 3));
+	}
+	
 
 	public boolean consultarIngresos(TipoCurso tipo, int anioInicio, int anioFin, int mesInicio, int mesFin) {
 		boolean valor = false;
@@ -139,6 +98,16 @@ public class GestorConsultasTest {
 	public boolean comprobarId(int id) {
 		boolean valor = false;
 		valor= (id>=0);
+		return valor;
+	}
+	
+	public boolean comProbarFecha(int anioInicio, int anioFin, int mesInicio, int mesFin) {
+		boolean valor = false;
+		SimpleDateFormat getYearFormat = new SimpleDateFormat("yyyy");	     
+	     Date fechaActual = new Date();
+	     String anioActual = getYearFormat.format(fechaActual);
+		valor = (anioInicio>= Integer.parseInt(anioActual) && anioFin>= Integer.parseInt(anioActual)
+	    		 && mesInicio != 8 && mesInicio != 7 && mesFin != 7 && mesFin != 8 && mesInicio <= 12 && mesFin <= 12);
 		return valor;
 	}
 	
