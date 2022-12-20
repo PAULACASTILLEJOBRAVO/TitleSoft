4b3790bd88a51a1bfce229b04d3d2ecdc63cc42a
Virginis <virginia.lopez9@alu.uclm.es>
2022-12-20 12:18:53 +0100

    gestor centro

--- TitulosPropiosUCLM/src/negocio/controllers/GestorCentro.java
+++ TitulosPropiosUCLM/src/negocio/controllers/GestorCentro.java
@@ -7,10 +7,9 @@ public class GestorCentro {
 
 
 	public static Centro seleccionarCentro(String id){
-		Centro centro = null;
 		if(id == "UCLM" || id == "UCM") {
 			CentroDAO centroDAO= new CentroDAO();
-			centro = centroDAO.seleccionarCentro(id);
+			Centro centro = centroDAO.seleccionarCentro(id);
 			System.out.println(centro);
 			return centro;
 		}
